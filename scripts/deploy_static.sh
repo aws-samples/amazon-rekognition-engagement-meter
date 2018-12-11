7560bf94e090d5f7961f13f20b0ddbee63281355
Matteo Figus <matteofigus@gmail.com>
2018-12-11 18:07:36 +0000

    More polishing, complete the deployment part

--- scripts/deploy_static.sh
+++ scripts/deploy_static.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws s3 cp ./src/static s3://matteo-rekognition-demo-test/static --recursive --acl public-read
+aws s3 cp ./src/static s3://matteo-rekognition-demo-test/static --recursive --acl public-read --exclude settings.js
--- src/functions/setup/s3-handler.js
+++ src/functions/setup/s3-handler.js
@@ -5,8 +5,15 @@ const {
   TO_BUCKET
 } = process.env;
 
-const STATIC_FILES = ["index.html", "style.css"];
-const CONFIG_FILENAME = "settings.json";
+const STATIC_FILES = [
+  "app.js",
+  "index.html",
+  "jpeg_camera_update_for_safari.js",
+  "jpeg_camera_with_dependencies.min.js",
+  "jpeg_camera.swf",
+  "style.css"
+];
+const CONFIG_FILENAME = "settings.js";
 
 module.exports = s3 => {
   const copyFile = params => s3.copyObject(params).promise();
@@ -14,36 +21,39 @@ module.exports = s3 => {
   const deleteFile = params => s3.deleteObject(params).promise();
 
   return {
-    copyFiles: () => Promise.all(
-      STATIC_FILES.map(file =>
-        copyFile({
+    copyFiles: () =>
+      Promise.all(
+        STATIC_FILES.map(file =>
+          copyFile({
+            ACL: "public-read",
+            Bucket: TO_BUCKET,
+            CopySource: `${FROM_BUCKET}/static/${file}`,
+            Key: file
+          })
+        )
+      ),
+
+    removeFiles: () =>
+      Promise.all(
+        [CONFIG_FILENAME, ...STATIC_FILES].map(file =>
+          deleteFile({
+            Bucket: TO_BUCKET,
+            Key: file
+          })
+        )
+      ),
+
+    writeSettings: () =>
+      s3
+        .putObject({
           ACL: "public-read",
           Bucket: TO_BUCKET,
-          CopySource: `${FROM_BUCKET}/static/${file}`,
-          Key: file
+          Key: CONFIG_FILENAME,
+          Body: `window.rekognitionSettings = ${JSON.stringify({
+            apiGateway: API_GATEWAY,
+            cognitoIdentityPool: COGNITO_IDENTITY_POOL
+          })};`
         })
-      )
-    ),
-
-    removeFiles: () => Promise.all(
-      [CONFIG_FILENAME, ...STATIC_FILES].map(file =>
-        deleteFile({
-          Bucket: TO_BUCKET,
-          Key: file
-        })
-      )
-    ),
-
-    writeSettings: () => s3
-      .putObject({
-        ACL: "public-read",
-        Bucket: TO_BUCKET,
-        Key: CONFIG_FILENAME,
-        Body: JSON.stringify({
-          apiGateway: API_GATEWAY,
-          cognitoIdentityPool: COGNITO_IDENTITY_POOL
-        })
-      })
-      .promise()
+        .promise()
   };
 };
