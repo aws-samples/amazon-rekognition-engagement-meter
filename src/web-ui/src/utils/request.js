ea7ece7deb1e1cb653e7e0332eae8a08e0f1205f
Matteo Figus <matteofigus@gmail.com>
2019-01-02 15:17:07 +0000

    Add auth and integrate Amplify

--- src/web-ui/src/utils/request.js
+++ src/web-ui/src/utils/request.js
@@ -1,9 +1,25 @@
-import axios from "axios";
+import Amplify, { API } from "aws-amplify";
+
+const region = window.rekognitionSettings.region || "eu-west-1";
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: window.rekognitionSettings.cognitoIdentityPool,
+    region
+  },
+  API: {
+    endpoints: [
+      {
+        name: "apiGateway",
+        endpoint: window.rekognitionSettings.apiGateway,
+        region
+      }
+    ]
+  }
+});
 
 export default (url, method, data) =>
-  axios({
-    data: data || undefined,
-    headers: { "Content-Type": "application/json" },
-    method: method || "get",
-    url: window.rekognitionSettings.apiGateway + url
+  API[method || "get"]("apiGateway", url, {
+    body: data || undefined,
+    headers: { "Content-Type": "application/json" }
   });
