6bae9f84d69f8a06019381c97b623a34d172db79
Matteo Figus <matteofigus@gmail.com>
2019-01-03 18:05:17 +0000

    Add architecture diagram and first go at README

--- scripts/deploy_cfn.sh
+++ scripts/deploy_cfn.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 aws cloudformation package --template-file ./src/cfn/template.yaml --s3-bucket rekognition-engagement-meter --output-template-file ./dist/template.yaml
+aws s3 cp ./dist/template.yaml s3://rekognition-engagement-meter/ --acl public-read
 aws cloudformation deploy --template-file ./dist/template.yaml --region eu-west-1 --capabilities CAPABILITY_IAM --parameter-overrides CollectionId=RekoDemo --stack-name rekodemo
