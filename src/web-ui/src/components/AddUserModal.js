8fd0f5ac729e60958381cbf2819f08caedc1a427
Matteo Figus <matteofigus@gmail.com>
2019-03-07 16:03:38 +0000

    Move 2 components to hooks

--- src/web-ui/src/components/AddUserModal.js
+++ src/web-ui/src/components/AddUserModal.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   Alert,
   Button,
@@ -8,176 +8,145 @@ import {
   Modal
 } from "react-bootstrap";
 
-class AddUserModal extends Component {
-  constructor(props, context) {
-    super(props, context);
+export default props => {
+  const [show, setShow] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [image, setImage] = useState(undefined);
+  const [formState, setFormState] = useState("initial");
+  const saveHandler = props.onSave;
 
-    this.getValidationState = this.getValidationState.bind(this);
-    this.processImage = this.processImage.bind(this);
-    this.submitForm = this.submitForm.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.saveHandler = props.onSave;
+  const getValidationState = value => (!value ? "error" : "success");
 
-    this.state = {
-      show: false,
-      fullName: "",
-      jobTitle: "",
-      image: undefined,
-      formState: "initial"
-    };
-  }
-
-  getValidationState(field) {
-    return !this.state[field] ? "error" : "success";
-  }
-
-  processImage(file) {
+  const processImage = file => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = () => this.setState({ image: reader.result.split(",")[1] });
-    reader.onerror = () => this.setState({ formState: "error" });
-  }
+    reader.onload = () => setImage(reader.result.split(",")[1]);
+    reader.onerror = () => setFormState("error");
+  };
 
-  submitForm(e) {
-    this.setState({ formState: "saving" });
+  const submitForm = e => {
+    setFormState("saving");
     e.preventDefault();
-    this.saveHandler({
-      fullName: this.state.fullName,
-      jobTitle: this.state.jobTitle,
-      image: this.state.image
-    })
-      .then(() => this.setState({ formState: "saved" }))
-      .catch(() => this.setState({ formState: "error" }));
-  }
+    saveHandler({ fullName, jobTitle, image })
+      .then(() => setFormState("saved"))
+      .catch(() => setFormState("error"));
+  };
 
-  toggle(reset) {
+  const toggle = reset => {
+    setShow(!show);
     if (reset) {
-      this.setState({
-        formState: "initial",
-        show: !this.state.show,
-        fullName: "",
-        jobTitle: "",
-        image: undefined
-      });
-    } else this.setState({ show: !this.state.show });
-  }
+      setFormState("initial");
+      setFullName("");
+      setJobTitle("");
+      setImage(undefined);
+    }
+  };
 
-  render() {
-    return (
-      <>
-        <Button
-          bsStyle="warning"
-          onClick={() => this.toggle(true)}
-          style={{ marginLeft: "20px" }}
-        >
-          Add a new user
-        </Button>
+  return (
+    <>
+      <Button
+        bsStyle="warning"
+        onClick={() => toggle(true)}
+        style={{ marginLeft: "20px" }}
+      >
+        Add a new user
+      </Button>
 
-        <Modal
-          show={this.state.show}
-          onHide={this.toggle}
-          style={{ color: "#000" }}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Add a new user</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p>
-              By uploading a picture and associating it with a name, Amazon
-              Rekognition can recognize that person.
-            </p>
-            <hr />
-            <Alert
-              bsStyle="warning"
-              style={{
-                display: this.state.formState === "saving" ? "block" : "none"
-              }}
+      <Modal show={show} onHide={toggle} style={{ color: "#000" }}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add a new user</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            By uploading a picture and associating it with a name, Amazon
+            Rekognition can recognize that person.
+          </p>
+          <hr />
+          <Alert
+            bsStyle="warning"
+            style={{
+              display: formState === "saving" ? "block" : "none"
+            }}
+          >
+            Please wait
+          </Alert>
+          <Alert
+            bsStyle="danger"
+            style={{
+              display: formState === "error" ? "block" : "none"
+            }}
+          >
+            An error happened. Retry.
+          </Alert>
+          <Alert
+            bsStyle="success"
+            style={{
+              display: formState === "saved" ? "block" : "none"
+            }}
+          >
+            The user has been added.
+          </Alert>
+          <form
+            style={{
+              display: formState === "initial" ? "block" : "none"
+            }}
+          >
+            <FormGroup
+              controlId="fullName"
+              validationState={getValidationState(fullName)}
             >
-              Please wait
-            </Alert>
-            <Alert
-              bsStyle="danger"
-              style={{
-                display: this.state.formState === "error" ? "block" : "none"
-              }}
+              <ControlLabel>Full name</ControlLabel>
+              <FormControl
+                type="text"
+                value={fullName}
+                placeholder="Full Name e.g. Jane Doe"
+                onChange={e => setFullName(e.target.value)}
+              />
+              <FormControl.Feedback />
+            </FormGroup>
+            <FormGroup
+              controlId="jobTitle"
+              validationState={getValidationState(jobTitle)}
             >
-              An error happened. Retry.
-            </Alert>
-            <Alert
-              bsStyle="success"
-              style={{
-                display: this.state.formState === "saved" ? "block" : "none"
-              }}
+              <ControlLabel>Job Title</ControlLabel>
+              <FormControl
+                type="text"
+                value={jobTitle}
+                placeholder="Job Title e.g. CEO"
+                onChange={e => setJobTitle(e.target.value)}
+              />
+              <FormControl.Feedback />
+            </FormGroup>
+            <FormGroup
+              controlId="image"
+              validationState={getValidationState(image)}
             >
-              The user has been added.
-            </Alert>
-            <form
-              style={{
-                display: this.state.formState === "initial" ? "block" : "none"
-              }}
-            >
-              <FormGroup
-                controlId="fullName"
-                validationState={this.getValidationState("fullName")}
-              >
-                <ControlLabel>Full name</ControlLabel>
-                <FormControl
-                  type="text"
-                  value={this.state.fullName}
-                  placeholder="Full Name e.g. Jane Doe"
-                  onChange={e => this.setState({ fullName: e.target.value })}
-                />
-                <FormControl.Feedback />
-              </FormGroup>
-              <FormGroup
-                controlId="jobTitle"
-                validationState={this.getValidationState("jobTitle")}
-              >
-                <ControlLabel>Job Title</ControlLabel>
-                <FormControl
-                  type="text"
-                  value={this.state.jobTitle}
-                  placeholder="Job Title e.g. CEO"
-                  onChange={e => this.setState({ jobTitle: e.target.value })}
-                />
-                <FormControl.Feedback />
-              </FormGroup>
-              <FormGroup
-                controlId="image"
-                validationState={this.getValidationState("image")}
-              >
-                <ControlLabel>Photo</ControlLabel>
-                <FormControl
-                  type="file"
-                  value={this.state.imge}
-                  onChange={e => this.processImage(e.target.files[0])}
-                  id="image"
-                />
-                <FormControl.Feedback />
-              </FormGroup>
-            </form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button
-              onClick={this.submitForm}
-              bsStyle="primary"
-              type="submit"
-              disabled={
-                !this.state.fullName ||
-                !this.state.jobTitle ||
-                !this.state.image ||
-                this.state.formState !== "initial"
-              }
-              show="false"
-            >
-              Add User
-            </Button>
-            <Button onClick={this.toggle}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
-}
-
-export default AddUserModal;
+              <ControlLabel>Photo</ControlLabel>
+              <FormControl
+                type="file"
+                onChange={e => processImage(e.target.files[0])}
+                id="image"
+              />
+              <FormControl.Feedback />
+            </FormGroup>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={submitForm}
+            bsStyle="primary"
+            type="submit"
+            disabled={
+              !fullName || !jobTitle || !image || formState !== "initial"
+            }
+            show="false"
+          >
+            Add User
+          </Button>
+          <Button onClick={toggle}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
